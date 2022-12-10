1ae677641d2e2d04f0749c3e24d7a2bfbd18429e
Raymond Purba <114927387+raydnom@users.noreply.github.com>
2022-12-10 22:10:32 +0700

    Add files via upload

---
+++ annual_payment_type_usage.sql
@@ -0,0 +1,20 @@
+with payment AS(
+SELECT
+        date_part('year',o.order_purchase_timestamp) as year,
+        op.payment_type,
+        COUNT (order_purchase_timestamp) as payment_used
+FROM order_payments_dataset op
+JOIN orders_dataset o ON o.order_id = op.order_id
+GROUP BY year,payment_type
+ORDER BY year
+)
+SELECT
+     payment_type,
+     SUM(CASE WHEN year = '2016' THEN payment_used ELSE 0 END) AS yr_2016,
+     SUM(CASE WHEN year = '2017' THEN payment_used ELSE 0 END) AS yr_2017,
+     SUM(CASE WHEN year = '2018' THEN payment_used ELSE 0 END) AS yr_2018,
+	 SUM(payment_used) AS total_usage
+FROM payment
+GROUP BY payment_type
+ORDER BY total_usage DESC;
+
